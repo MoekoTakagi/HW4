9b0f86d852a4eb713a5cb0d2c30b3fbe3ba67362
高木 萌子 <barbiepinkmindoll@gmail.com>
2015-06-24 18:46:41 +0900

    実行時エラー

---
+++ IsPrimeTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IsPrimeTest {
+	@Test
+	public void test(){
+//		assertFalse(IsPrime.isPrime(1));
+//		assertTrue(IsPrime.isPrime(2));
+		assertTrue(true);
+//		assertFalse(IsPrime.isPrime(4));
+//		assertTrue(IsPrime.isPrime(65537));
+	}
+}
