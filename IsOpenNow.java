86b316ee6f47a0a9b2d90dfea713af202aa5128f
高木 萌子 <barbiepinkmindoll@gmail.com>
2015-06-26 02:41:48 +0900

    課題2

--- IsOpenNow.java
+++ IsOpenNow.java
@@ -1,31 +1,25 @@
+package openNow;
+
 import java.util.*;
 import java.io.*;
 public class IsOpenNow {
-	static Calendar calendar;
-	static int dayOfWeek;
-	static int hour;
-	static String[] sun;
-	static String[] mon;
-	static String[] tue;
-	static String[] wed;
-	static String[] thu;
-	static String[] fri;
-	static String[] sat;
-
-	public static void main(String[] args){
-		calendar = Calendar.getInstance();
-		dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-		hour = calendar.get(Calendar.HOUR_OF_DAY);
-
-		boolean x = isOpenNow(Calendar.SUNDAY, 13);
-		System.out.println(x);
+	public static boolean isOpenNow(){
+		Calendar calendar = Calendar.getInstance();
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+		int hour = calendar.getMaximum(Calendar.HOUR_OF_DAY);
+		return isOpenNow(dayOfWeek, hour);
 	}
-
 	public static boolean isOpenNow(int dayOfWeek, int hour){
 		int start;
 		int end;
-
-
+		String[] sun = null;
+		String[] mon = null;
+		String[] tue = null;
+		String[] wed = null;
+		String[] thu = null;
+		String[] fri = null;
+		String[] sat = null;
+		
 		try{
 			BufferedReader reader = new BufferedReader(new FileReader("/Users/takagimoeko/open.txt"));
 			String line;
@@ -55,33 +49,55 @@ public class IsOpenNow {
 		if(dayOfWeek == 1 && !sun[1].equals("holiday")){
 			start = Integer.parseInt(sun[1]);
 			end = Integer.parseInt(sun[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 2 && !mon[1].equals("holiday")){
 			start = Integer.parseInt(mon[1]);
 			end = Integer.parseInt(mon[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 3 && !tue[1].equals("holiday")){
 			start = Integer.parseInt(tue[1]);
 			end = Integer.parseInt(tue[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 4 && !wed[1].equals("holiday")){
 			start = Integer.parseInt(wed[1]);
 			end = Integer.parseInt(wed[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 5 && !thu[1].equals("holiday")){
 			start = Integer.parseInt(thu[1]);
 			end = Integer.parseInt(thu[1]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 6 && !fri[1].equals("holiday")){
 			start = Integer.parseInt(fri[1]);
 			end = Integer.parseInt(fri[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}else if(dayOfWeek == 7 && !sat[1].equals("holiday")){
 			start = Integer.parseInt(sat[1]);
 			end = Integer.parseInt(sat[3]);
+			if(start > end){
+				return (hour >= start && hour <=24) || (hour <= end && hour >= 0); 
+			}
 			return hour >= start && hour <= end;
 		}
 		return false;
 
 	}
+
 }
